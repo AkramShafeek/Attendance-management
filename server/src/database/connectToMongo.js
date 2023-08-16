b3ef7bdc3bf3e5b9a2bd389d00d07e6848c89d7f
Akram <akramshafeek70@gmail.com>
2023-08-16 23:03:42 +0530

    Migrated server api to separate repo

--- server/src/database/connectToMongo.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectToMongo = (MONGO_URI) => {
-    return mongoose.connect(MONGO_URI);
-};
-
-module.exports = connectToMongo;
--- server/src/middlewares/emptyReqBodyValidator.js
+++
@@ -1,8 +0,0 @@
-const emptyReqBodyValidator = (req, res, next) => {    
-    if (!req.body || Object.keys(req.body).length === 0) {        
-        throw new Error("Empty request body");
-    }
-    next();
-}
-
-module.exports = emptyReqBodyValidator;
